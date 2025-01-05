429ab0ec6c64d95553cf8949fd3144a099884027
INJOON CHOI <dlswns8679@naver.com>
2025-01-06 00:34:32 +0900

    first project - 초기 설정

---
+++ src/main/java/com/antique/AntiqueApplication.java
@@ -0,0 +1,11 @@
+package com.antique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AntiqueApplication.class, args);
+	}
+}
---
+++ src/test/java/com/antique/AntiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
