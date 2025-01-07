21a86cb15228c88249819901111ac0b5ef95f159
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-08 00:48:42 +0900

    feat #7 : Service 코드 작성

---
+++ src/main/java/com/antique/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.antique.service;
+
+import com.antique.domain.Product;
+import com.antique.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
