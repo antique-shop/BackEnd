ffed85c29c2d409a95d84e5605c10a714fb35281
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-08 21:27:09 +0900

    feat #7 : ProductDTO 반환하도록 변경

--- src/main/java/com/antique/controller/ProductController.java
+++ src/main/java/com/antique/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.antique.controller;
 
 import com.antique.domain.Product;
+import com.antique.dto.ProductDTO;
 import com.antique.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -21,7 +23,7 @@ public class ProductController {
     상품 전체 목록 조회
      */
     @GetMapping("/getProducts")
-    public List<Product> getProducts() {
+    public List<ProductDTO> getProducts() {
         return productService.getAllProducts();
     }
 }
