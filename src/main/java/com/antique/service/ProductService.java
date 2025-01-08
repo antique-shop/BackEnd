93f67f53e741b613dc0c7eff590b38d7a7739aa5
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-08 21:27:15 +0900

    feat #7 : ProductDTO 반환하도록 변경

--- src/main/java/com/antique/service/ProductService.java
+++ src/main/java/com/antique/service/ProductService.java
@@ -1,12 +1,14 @@
 package com.antique.service;
 
 import com.antique.domain.Product;
+import com.antique.dto.ProductDTO;
 import com.antique.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +16,19 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getAllProducts() {
-        return productRepository.findAll();
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(product -> new ProductDTO(
+                        product.getProductId(),
+                        product.getName(),
+                        product.getDescription(),
+                        product.getPrice(),
+                        product.getStatus().toString(),
+                        product.getProductImage(),
+                        product.getSeller().getNickname()
+                ))
+                .collect(Collectors.toList());
     }
 }
