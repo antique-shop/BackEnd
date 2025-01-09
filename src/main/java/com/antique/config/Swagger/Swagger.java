a10055266a5f2a235c0e4a4c317f829bfa0a14be
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-09 13:34:00 +0900

    feat #7 : Swagger 설정을 위한 Config 파일 추가

---
+++ src/main/java/com/antique/config/Swagger/Swagger.java
@@ -0,0 +1,18 @@
+package com.antique.config.Swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(info);
+    }
+
+    Info info = new Info().title("Antique").version("0.0.1").description(
+            "프로젝트 [Antique]");
+}
