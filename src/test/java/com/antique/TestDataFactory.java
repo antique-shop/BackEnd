e400f7385895bd5d28629e23ac5b6724cfef306d
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-18 16:21:44 +0900

    refactor #30 : 리뷰 반영 (default 메소드 추가)

--- src/test/java/com/antique/TestDataFactory.java
+++ src/test/java/com/antique/TestDataFactory.java
@@ -14,10 +14,26 @@ public class TestDataFactory {
                 .build();
     }
 
+    public static User createUserWithDefault(Long userId) {
+        return User.builder()
+                .userId(userId)
+                .email("default@example.com")
+                .nickname("defaultNickname")
+                .address("defaultAddress")
+                .build();
+    }
+
     public static UserRequestDTO createUserRequestDTO(String nickname, String address) {
         return UserRequestDTO.builder()
                 .nickname(nickname)
                 .address(address)
+                .build();
+    }
+
+    public static UserRequestDTO createUserRequestDTOWithDefaults() {
+        return UserRequestDTO.builder()
+                .nickname("defaultNickname")
+                .address("defaultAddress")
                 .build();
     }
 }
