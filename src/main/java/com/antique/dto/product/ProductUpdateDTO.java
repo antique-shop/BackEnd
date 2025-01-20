472306da0a0f70ad385b47918183eabd6bc2d8d7
INJOON CHOI <dlswns8679@naver.com>
2025-01-20 16:51:03 +0900

    refactor #34 : dto 코드 개발

---
+++ src/main/java/com/antique/dto/product/ProductUpdateDTO.java
@@ -0,0 +1,33 @@
+package com.antique.dto.product;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ProductUpdateDTO {
+
+    @Schema(description = "user id", example = "1", required = true)
+    private Long userId;
+
+    @Schema(description = "product id", example = "1", required = true)
+    private Long productId;
+
+    @Schema(description = "상품명", example = "Vintage Pants", required = true)
+    private String name;
+
+    @Schema(description = "상품 설명", example = "A beautiful vintage pants", required = true)
+    private String description;
+
+    @Schema(description = "상품 가격", example = "120,000", required = true)
+    private int price;
+
+    @Schema(description = "카테고리 ID", example = "1", required = true)
+    private Long categoryId;
+
+    @Schema(description = "상품 이미지 URL 리스트", example = "[\"https://example.com/image1.jpg\", \"https://example.com/image2.jpg\"]", required = false)
+    private List<String> images;
+}
