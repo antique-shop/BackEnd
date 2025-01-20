9a78a5b05975df0cf7e45bd42832a0c6a953ddae
INJOON CHOI <dlswns8679@naver.com>
2025-01-20 16:50:38 +0900

    feature #34 : 상품 수정 서비스 코드 개발

--- src/main/java/com/antique/service/user/UserProductService.java
+++ src/main/java/com/antique/service/user/UserProductService.java
@@ -4,8 +4,12 @@ import com.antique.domain.Category;
 import com.antique.domain.Product;
 import com.antique.domain.ProductImage;
 import com.antique.domain.User;
+import com.antique.dto.ProductDTO;
 import com.antique.dto.product.ProductRequestDTO;
+import com.antique.dto.product.ProductUpdateDTO;
 import com.antique.exception.category.CategoryNotFoundException;
+import com.antique.exception.product.ProductErrorCode;
+import com.antique.exception.product.ProductNotFoundException;
 import com.antique.exception.user.UserNotFoundException;
 import com.antique.repository.CategoryRepository;
 import com.antique.repository.ProductRepository;
@@ -65,5 +69,24 @@ public class UserProductService {
 
         // 5. productId만 반환
         return savedProduct.getProductId();
+    }
+
+    @Transactional
+    public Long updateProduct(ProductUpdateDTO request) {
+        // 1. 상품 확인
+        Product product = productRepository.findById(request.getProductId())
+                .orElseThrow(() -> new ProductNotFoundException(ProductErrorCode.PRODUCT_NOT_FOUND));
+        // 2. 판매자 확인
+        User seller = userRepository.findById(request.getUserId())
+                .orElseThrow(UserNotFoundException::new);
+        // 3. 카테고리 확인
+        Category category = categoryRepository.findById(request.getCategoryId())
+                .orElseThrow(CategoryNotFoundException::new);
+        // 4. Product 엔티티의 수정 메서드 호출
+        product.updateFromDTO(request, category, seller);
+
+        productRepository.save(product);
+
+        return product.getProductId();
     }
 }
