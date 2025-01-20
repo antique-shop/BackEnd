beb4425cd3dd92b0bfaaa614cbf90efe2a36cafc
INJOON CHOI <dlswns8679@naver.com>
2025-01-21 00:13:34 +0900

    refactor #37 : 충돌 해결

--- src/main/java/com/antique/service/ProductService.java
+++ src/main/java/com/antique/service/ProductService.java
@@ -71,6 +71,7 @@ public class ProductService {
         // 5. productId만 반환
         return savedProduct.getProductId();
     }
+    
     @Transactional
     public Long updateProduct(ProductUpdateDTO request) {
         // 1. 상품 확인
@@ -89,6 +90,7 @@ public class ProductService {
 
         return product.getProductId();
     }
+    
     /*
     상품 전체 목록 조회
     */
