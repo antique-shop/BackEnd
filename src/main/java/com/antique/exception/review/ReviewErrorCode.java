494d00f84c115b97fb54dfabdf87a505e88f7a4f
minjgziii <104371003+minjgziii@users.noreply.github.com>
2025-01-24 01:26:09 +0900

    feat #48 : 리뷰 수정 및 삭제 관련 에러 코드 추가

--- src/main/java/com/antique/exception/review/ReviewErrorCode.java
+++ src/main/java/com/antique/exception/review/ReviewErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ReviewErrorCode {
-    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자의 리뷰가 존재하지 않습니다.");
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자의 리뷰가 존재하지 않습니다."),
+    REVIEW_IS_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 리뷰가 존재하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
