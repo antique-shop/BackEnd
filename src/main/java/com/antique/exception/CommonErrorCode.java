cac9d3967c238a12e63ac0856c3799a42e3d5e51
INJOON CHOI <dlswns8679@naver.com>
2025-01-29 19:06:24 +0900

    feature #56 : 구글 로그인 구현 - config 파일 생성

--- src/main/java/com/antique/exception/CommonErrorCode.java
+++ src/main/java/com/antique/exception/CommonErrorCode.java
@@ -21,7 +21,8 @@ public enum CommonErrorCode implements ErrorCode {
     REVIEW_IS_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 리뷰가 존재하지 않습니다."),
     // 기타
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
-    GOOGLE_TOKEN_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "Google Access Token 요청에 실패했습니다.");
+    GOOGLE_TOKEN_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "Google Access Token 요청에 실패했습니다."),
+    GOOGLE_REDIRECT_FAILED(HttpStatus.BAD_REQUEST, "Google 리다이렉트 요청에 실패했습니다.");
 
 
     private final HttpStatus status;
--- src/main/java/com/antique/service/login/google/GoogleLoginService.java
+++ src/main/java/com/antique/service/login/google/GoogleLoginService.java
@@ -18,9 +18,11 @@ public class GoogleLoginService {
     private final UserRepository userRepository;
     private final JwtTokenGenerator jwtTokenGenerator;
 
-    public UserResponseDTO handleLoginOrRegister(GoogleAccountProfileResponse googleProfile) {
-        // 이메일을 기준으로 사용자 검색
-        AtomicBoolean isNewUser = new AtomicBoolean(false); // 회원가입 여부를 추적
+    /**
+     * Google OAuth 로그인 및 회원가입 처리 메서드
+     */
+    public UserResponseDTO loginOrRegisterUser(GoogleAccountProfileResponse googleProfile) {
+        AtomicBoolean isNewUser = new AtomicBoolean(false); // 회원가입 여부 추적
 
         User user = userRepository.findByEmail(googleProfile.getEmail())
                 .orElseGet(() -> {
@@ -35,6 +37,7 @@ public class GoogleLoginService {
         String message = isNewUser.get() ? "회원가입이 완료되었습니다." : "로그인 성공";
         int statusCode = isNewUser.get() ? HttpStatus.CREATED.value() : HttpStatus.OK.value();
 
+        // 임시적으로 userid에 null을 줌, 차차 userid를 제외하고 jwt 토큰을 request 파라미터로 주도록 바꾸어 나가야 함
         return new UserResponseDTO(null, message, statusCode, jwtToken);
     }
 
