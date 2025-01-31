37a9d6733663cbf712b7078d587c048fa939b4a0
INJOON CHOI <dlswns8679@naver.com>
2025-02-01 01:22:06 +0900

    feat #64 : access token 갱신 api 추가

--- src/main/java/com/antique/exception/CommonErrorCode.java
+++ src/main/java/com/antique/exception/CommonErrorCode.java
@@ -22,7 +22,9 @@ public enum CommonErrorCode implements ErrorCode {
     // 기타
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
     GOOGLE_TOKEN_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "Google Access Token 요청에 실패했습니다."),
-    GOOGLE_REDIRECT_FAILED(HttpStatus.BAD_REQUEST, "Google 리다이렉트 요청에 실패했습니다.");
+    GOOGLE_REDIRECT_FAILED(HttpStatus.BAD_REQUEST, "Google 리다이렉트 요청에 실패했습니다."),
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 토큰입니다."),
+    REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "Refresh Token이 일치하지 않습니다.");
 
 
     private final HttpStatus status;
